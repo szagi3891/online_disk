2efbdacf1497c3c8a7988dab28a2b29f7bfd1aea
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2017-12-18 06:31:55 +0100

    poprawka w funkcji zapisującej bloba

--- src_backend/src/blob/fs/save_file.rs
+++ src_backend/src/blob/fs/save_file.rs
@@ -3,8 +3,11 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::io::Result;
 use std::fs;
+use std::io::ErrorKind;
 
-pub fn save_file(path: &Path, content: &[u8]) -> Result<()> {
+use blob::fs::get_file::get_file;
+
+fn save_file_inner(path: &Path, content: &[u8]) -> Result<()> {
                     //https://doc.rust-lang.org/std/fs/struct.OpenOptions.html#method.create_new
 
     if let Some(parent) = path.parent() {
@@ -25,5 +28,29 @@ pub fn save_file(path: &Path, content: &[u8]) -> Result<()> {
             Ok(())
         },
         Err(err) => Err(err),
+    }
+}
+
+pub fn save_file(path: &Path, content: &[u8]) -> Result<()> {
+
+    match save_file_inner(path, content) {
+        Ok(()) => Ok(()),
+        Err(err) => {
+            if err.kind() == ErrorKind::AlreadyExists && verify(path, content) {
+                return Ok(());
+            }
+
+            Err(err)
+        }
+    }
+}
+
+fn verify(path: &Path, content: &[u8]) -> bool {
+
+    match get_file(path) {
+        Some(buf) => buf.as_slice() == content,
+        None => {
+            panic!("Nigdy program nie powinien wejść w to odgałęzienie");
+        },
     }
 }
