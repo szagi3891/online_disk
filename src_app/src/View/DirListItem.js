db02593e30f290c562f64263595f3b05ca230973
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2018-04-30 07:31:33 +0200

    lista katalogów

--- src_app/src/View/DirListItem.js
+++ src_app/src/View/DirListItem.js
@@ -5,13 +5,33 @@ import { Map as IMap } from 'immutable';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import glamorous from 'glamorous';
+import rgba from 'hex-rgba';
 import { Store } from '../Store';
 import type { NodeItemType } from '../Store/Type';
 import { DirItem } from '../Store/Root/DirItem';
 import { FileItem } from '../Store/Root/FileItem';
 
+const backgroundColor = '#e0e0e0';
+
 const Main = glamorous.div({
-    cursor: 'pointer'
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingLeft: '5px',
+    paddingRight: '5px',
+    cursor: 'pointer',
+    backgroundColor: backgroundColor,
+    marginBottom: '3px',
+    border: '1px solid transparent',
+    ':hover': {
+        border: '1px solid blue',
+        backgroundColor: rgba(backgroundColor, 50)
+    }
+});
+
+const OptionDiv = glamorous.div({
+    ':hover': {
+        color: 'blue'
+    }
 });
 
 type DirListItemPropsType = {|
@@ -24,15 +44,37 @@ type DirListItemPropsType = {|
 export class DirListItem extends React.Component<DirListItemPropsType> {
     render(): React.Node {
         const { name } = this.props;
-        return (
-            <Main onClick={this._onClick}>
-                <div>name: {name !== null ? name : '..'}</div>
-            </Main>
-        )
+        if (name === null) {
+            return (
+                <Main onClick={this._onClick}>
+                    { this._renderName('..') }
+                </Main>
+            );
+        } else {
+            return (
+                <Main onClick={this._onClick}>
+                    { this._renderName(name) }
+                    { this._renderDeleteOption() }
+                </Main>
+            );
+        }
+    }
+
+    _renderName(name: string) {
+        return <div>{name}</div>;
+    }
+
+    _renderDeleteOption() {
+        return <OptionDiv onClick={this._onDelete}>Delete</OptionDiv>;
     }
 
     _onClick = () => {
         const { store, node } = this.props;
         store.path.goTo(node.path);
+    }
+
+    _onDelete = (event: SyntheticEvent<>) => {
+        event.stopPropagation();
+        console.info('Kasuję element', this.props.name);
     }
 }
